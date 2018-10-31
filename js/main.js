b708d40b6005cd3375a57f394429b0394304a2a2
Raul de la Paz <rauldelapaznavarro@gmail.com>
2018-10-31 16:57:25 +0100

    Portafolio : Funcionalidad básica. Los enlaces del menú inferior no funcionan todavía

--- js/main.js
+++ js/main.js
@@ -4,8 +4,24 @@ $(document).ready(function () {
         anchors: ['pag1', 'pag2', 'pag3'],
         menu: '#menu'
     });
+
+    /**
+     * Evento que añade la clase hover a un elemento de menú al pasar el ratón por encima del padre de dicho elemento
+     */
+    $('[data-menuanchor]').hover(function (evento) {
+        let linkItem = $(this).find('a');
+        linkItem.toggleClass("hover");
+    })
+
+    $('[data-menuanchor]').on('click', irAAncla);
+
 })
 
-//TODO: listeners para que los textos de otras paginas no se muestren
-
-
+/**
+ * Función que recoge un evento de click en un li de menú y redirecciona con el href que contiene el elemento a hijo
+ * @param evento
+ */
+function irAAncla(evento) {
+    var aaa = $(this);
+    window.location.replace(aaa.children().attr("href"));
+}
