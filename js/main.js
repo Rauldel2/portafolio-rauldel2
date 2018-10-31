2a9f0f0f0baa1c39322a1007f591c76bf2d23b35
Raul de la Paz <rauldelapaznavarro@gmail.com>
2018-10-31 15:06:55 +0100

    Portafolio : Maquetar, añadir jquery y pagepiling.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    // Plugin PagePiling
+    $("#pagepiling").pagepiling({
+        anchors: ['pag1', 'pag2', 'pag3'],
+        menu: '#menu'
+    });
+})
+
+//TODO: listeners para que los textos de otras paginas no se muestren
+
+
